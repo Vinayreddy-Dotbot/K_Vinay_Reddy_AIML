543cd852506f4301e8d92c3a3207a5a4b067ebf4
K Vinay Reddy <vinayreddy3377@gmail.com>
2024-12-07 12:18:12 +0530

    Update program8.c

--- day-3/program8.c
+++ day-3/program8.c
@@ -5,10 +5,10 @@ int main()
     int i,j,num;
     printf("Enter an number:");
     scanf("%d",&num);
-    for(i=10;i>=1;i--)
+    for(i=num;i>=1;i--)
         {
             printf("multiplication table for %d:\n",i);
-        for(j=10;j>=1;j--)
+        for(j=num;j>=1;j--)
             printf("%dX%d=%d\n",i,j,i*j);
         }
 
