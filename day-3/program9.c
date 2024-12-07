36ad55b33231e0612cde202eb5c86612161f0b09
K Vinay Reddy <vinayreddy3377@gmail.com>
2024-12-07 14:53:37 +0530

    Add files via upload

---
+++ day-3/program9.c
@@ -0,0 +1,20 @@
+// to display more times without duplicating code
+#include<stdio.h>
+int main()
+{
+    void display()
+    {
+        printf("c\n");
+        printf("c++\n");
+        printf("Java\n");
+        printf("Python\n");
+        printf("SQL\n");
+        printf("Javascript\n");
+    }
+    display();
+    display();
+    display();
+    display();
+    display();
+    display();
+}
